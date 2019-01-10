9a0f6eeb7d48e1ca93cb036dce904f1b3fbbded2
GaryLougheed <glougheed@nevada.unr.edu>
2019-01-10 10:01:42 -0800

    adding startup files for 2DArrayHourGlass

---
+++ 2DArrayHourGlass/main.c
@@ -0,0 +1,19 @@
+// Header
+#include <iostream>
+
+using namespace std;
+
+// Function Prototypes
+
+
+// main
+int main()
+{
+
+  cout << "Hello, World" << '\n';
+
+  return 0;
+
+}
+
+// Supporting Function Implementation
